c1122fa98db9f7f6ce46fb8b62ad3d2c3d34edac
Артём Царёв Сергеевич <TsarevArr@MacBook-Pro.local>
2020-09-06 13:46:32 +0400

    final

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 
 * [Ссылка на макет в Figma](https://www.figma.com/file/StZjf8HnoeLdiXS7dYrLAh/JavaScript.-Sprint-4)
 
-* [ссылка на проект]();
+* [ссылка на проект](https://artemtsarev.github.io/mesto/);
 
 
 
--- main.js
+++ main.js
@@ -1,36 +1,33 @@
 let profile = document.querySelector(".profile");
 let profile_name = profile.querySelector(".profile__title");
-let profile_copyright = profile.querySelector(".profile__copyright");
+let profile_copyright = profile.querySelector(".profile__subtitle");
 let formElement = document.querySelector(".form__element");
 // О том, как это делать, расскажем позже.
 let profileEdit_btn = profile.querySelector(".profile__edit-button");
 let model = document.querySelector(".model");
 // Находим поля формы в DOM
-let nameInput = model.querySelector(".model__username").value;
-let jobInput = model.querySelector(".model__copyright").value;
+let nameInput = model.querySelector(".model__username");
+let jobInput = model.querySelector(".model__copyright");
+let modelClose = document.querySelector(".model__close");
 
 model.classList.add("model-hide");
 // Выберите элементы, куда должны быть вставлены значения полей
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-    // Вставьте новые значения с помощью textContent
-    profile_name.textContent = nameInput;
-    profile_copyright.textContent = jobInput;
-}
+
 
 function edit_profile(){
     model.classList.add("model-block");
-    model.classList.remove("model-hide");
+    nameInput.value = profile_name.textContent;
+    jobInput.value = profile_copyright.textContent;
 }
 
 function model_hide(){
     model.classList.remove("model-block");
 }
 
-function end(){
+function end(submitEvent){
+    submitEvent.preventDefault();
     model.classList.remove("model-block");
     profile_name.textContent = nameInput.value;
     profile_copyright.textContent = jobInput.value;
@@ -38,7 +35,7 @@ function end(){
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
 profileEdit_btn.addEventListener("click", edit_profile);
 modelClose.addEventListener("click", model_hide);
-formElement.addEventListener('click', end);
+formElement.addEventListener("submit", end);
+ 
